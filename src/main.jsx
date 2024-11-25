17db65c4e9b261c2d42faa24e02157e3275e46df
homenb1 <hmnbasumatary@gmail.com>
2024-11-25 22:46:31 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './store';
-import './index.css'; // Import your global styles
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App.jsx'
+import store from './store'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
-);
-
+  </React.StrictMode>,
+)
